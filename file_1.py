696e033ee66147d4163544df4ab123ed5c803455
v-roman <r.v.v.05112001@gmail.com>
2025-03-23 20:05:29 +0300

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,67 @@
+import file_operations
+from faker import Faker
+import random
+import os
+
+result = "result"
+os.makedirs(result, exist_ok=True)
+skills =[
+'Стремительный прыжок',
+'Электрический выстрел',
+'Ледяной удар',
+'Стремительный удар',
+'Кислотный взгляд',
+'Тайный побег',
+'Ледяной выстрел',
+'Огненный заряд'
+]
+letters_mapping = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+for i in range(10):
+    skill = random.sample(skills, 3)
+    first_skill, second_skill, third_skill = skill
+    for old, new in letters_mapping.items():
+        first_skill = first_skill.replace(old, new)
+        second_skill = second_skill.replace(old, new)
+        third_skill = third_skill.replace(old, new)
+    fake = Faker("ru_RU")
+    context = {
+  "first_name": fake.first_name(),
+  "last_name": fake.last_name(),
+  "town": fake.city(),
+  "job": fake.job(),
+  "strength": random.randint(3, 18),
+  "agility": random.randint(3, 18),
+  "endurance": random.randint(3, 18),
+  "intelligence": random.randint(3, 18),
+  "luck": random.randint(3, 18),
+  "skill_1": first_skill,
+  "skill_2": second_skill,
+  "skill_3": third_skill,
+    }
+    file_name = os.path.join(result, "form_{}.svg".format(i + 1))
+    file_operations.render_template("template.svg", file_name, context)
+
